0882a792e99798e18107c8640ecdf9ad2a5bcc46
deka108 <deka108@gmail.com>
2016-11-01 16:07:42 +0800

    AutoEncoder epochs

--- Part2-autoencoders/autoencoder_train.m
+++ Part2-autoencoders/autoencoder_train.m
@@ -3,36 +3,61 @@
 load 'dataTest.mat';
 load 'dataTrain.mat';
 
-%Take 100 samples for training and 20 for testing
-% dataTestSubset = dataTest(1, 1:20);
-% dataTestSubset = dataTest(1, :);
-% dataTrainSubset = dataTrain(1, 1:100);
-% dataTrainSubset = dataTrain(1, :);
+file_path = './Results/';
 
 hiddenSize1 = 100;
 
-autoenc1 = trainAutoencoder(dataTrain,hiddenSize1);
+epochs = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+[~, p] = size(epochs);
+mse_errors = zeros(p);
 
-figure(), plotWeights(autoenc1);
+for ep = 1:numel(epochs)
+    params = ['_', 'epoch', num2str(epochs(ep))];
+    
+    autoenc1 = trainAutoencoder(dataTrain, hiddenSize1, ...
+        'MaxEpochs', epochs(ep));
+    
+    f1 = figure();
+    weights_file_name = [file_path, 'weights/', 'autoenc1_weights', ...
+        params, '.jpg'];
+    plotWeights(autoenc1);
+    saveas(f1, weights_file_name);
 
-reconstructed = predict(autoenc1, dataTest);
+    reconstructed = predict(autoenc1, dataTest);
 
-mseError = 0;
-for i = 1:numel(dataTest)
-    mseError = mseError + mse(double(dataTest{1, i}) - reconstructed{1, i});
+    mseError = 0;
+    for i = 1:numel(dataTest)
+        mseError = mseError + mse(double(dataTest{1, i}) - reconstructed{1, i});
+    end
+
+    mseError = mseError/i;
+    mse_errors(ep) = mseError;
+    
+    disp(['mseError', params, ': ', num2str(mseError)]);
+
+    f2 = figure();
+    for i = 1:20
+        subplot(4,5,i);
+        imshow(dataTest{i});
+    end
+    dataset_file_name = [file_path, 'datatest/', ...
+            'autoenc1_datatest', params, '.jpg'];
+    saveas(f2, dataset_file_name);
+
+    f3 = figure();
+    for i = 1:20
+        subplot(4,5,i);
+        imshow(reconstructed{i});
+    end
+    reconstructed_file_name = [file_path, 'reconstructed/', ...
+            'autoenc1_reconstructed', params, '.jpg'];
+    saveas(f3, reconstructed_file_name);
 end
 
-mseError = mseError/i;
-
-figure;
-% for i = 1:20
-%     subplot(4,5,i);
-%     imshow(dataTest{i});
-% end
-% 
-% figure;
-% for i = 1:20
-%     subplot(4,5,i);
-%     imshow(reconstructed{i});
-% end
-
+error_txt = [file_path, 'errors/', 'autoenc1_epoch', '.txt'];
+fid = fopen(error_txt, 'wt');
+for ii = 1:size(mse_errors,1)
+    fprintf(fid,'%g\t',mse_errors(ii,:));
+    fprintf(fid,'\n');
+end
+fclose(fid);
